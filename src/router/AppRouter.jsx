edf01e1f91d67121db1767642d6c23dd7663dae9
FELIPE <tecnomin3@gmail.com>
2025-01-06 19:29:18 -0500

    deploy #8

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import { Route, Routes, Navigate, HashRouter } from 'react-router-dom';
-import AddUser from '../components/addUserComponents/AddUser';
-import UserList from '../components/userListComponent/UsersList';
+import React from "react";
+import { Route, Routes, Navigate, HashRouter } from "react-router-dom";
+import AddUser from "../components/addUserComponents/AddUser";
+import UserList from "../components/userListComponent/UsersList";
 
 const AppRouter = () => {
   return (
-    <HashRouter basename="/CRUD-Users-Management-App-Redux-ToolKit/">
+     // direccion local
+    //  <HashRouter >
+    <HashRouter basename="/LuisAcev/CRUD-Users-Management-App-Redux-ToolKit">
       <Routes>
         <Route path="/" element={<UserList />} />
         <Route path="/add" element={<AddUser />} />
